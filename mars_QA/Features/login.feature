e07c901963797c16b23aa3e5715689e7cc9ae76e
Jissa <jisaelza@gmail.com>
2021-08-26 21:22:55 +1000

    first commit

---
+++ mars_QA/Features/login.feature
@@ -0,0 +1,13 @@
+﻿Feature: login
+	Simple calculator for adding two numbers
+
+@mytag
+
+
+Scenario: Successful Login with valid Credentials
+	Given user is on home page
+	When user clicks Signin
+	Then  user enters 'jisaelza@gmail.com' and 'EA6qqHxnsrJv6HF'
+	And  user clicks on login
+	Then login should be successful
+	
---
+++ mars_QA/Steps/LoginSteps.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace mars_QA.Features
+{
+    [Binding]
+    public class LoginSteps
+    {
+
+        String test_url = "http://192.168.99.100:5000/Home";
+        IWebDriver driver;
+
+        [Given(@"user is on home page")]
+        public void GivenUserIsOnHomePage()
+        {
+            driver = new ChromeDriver();
+            driver.Url = test_url;
+
+        }
+        
+        [When(@"user clicks Signin")]
+        public void WhenUserClicksSignin()
+        {
+            driver.FindElement(By.XPath("//A[@class='item'][text()='Sign In']")).Click();
+        }
+        
+        [Then(@"user enters '(.*)' and '(.*)'")]
+        public void ThenUserEntersAnd(string username, string password)
+        {
+            driver.FindElement(By.XPath("(//INPUT[@type='text'])[2]")).SendKeys(username);
+            driver.FindElement(By.XPath("//INPUT[@type='password']")).SendKeys(password); 
+        }
+        
+        [Then(@"user clicks on login")]
+        public void ThenUserClicksOnLogin()
+        {
+            driver.FindElement(By.XPath("//BUTTON[@class='fluid ui teal button'][text()='Login']")).Click();
+        }
+        
+        [Then(@"login should be successful")]
+        public void ThenLoginShouldBeSuccessful()
+        {
+            Console.WriteLine("test passed");
+        }
+    }
+}
